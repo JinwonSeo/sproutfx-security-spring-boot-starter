e374b8965bc1d7acaee30e6b440925d6147cb85a
changUk5@lotte.net <changUk5@lotte.net>
2022-02-15 16:45:19 +0900

    [add] init project

---
+++ src/main/java/kr/sproutfx/common/sproutfxcommonsecurityspringbootstarter/SproutfxCommonSecuritySpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package kr.sproutfx.common.sproutfxcommonsecurityspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SproutfxCommonSecuritySpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SproutfxCommonSecuritySpringBootStarterApplication.class, args);
+	}
+
+}
